320214e79eafb2d68ed04014649d04d61969511e
devluanfagioni <devluanfagioni@gmail.com>
2026-02-11 22:21:02 -0300

    :)

--- 5) Desafio/main.js
+++ 5) Desafio/main.js
@@ -5,3 +5,32 @@
 // PERCORRAR PELA LISTA PARA CALCULAR A MÉDIA DO ALUNO
 // PEGUE A MEDIA E VERIFIQUE SE O ALUNO FOI APROVADO, REPROVADO OU ESTÁ DE RECUPERAÇÃO E IMPRIMA O RESULTADO NO CONSOLE
 // A MÉDIA PARA APROVAÇÃO É 7, PARA RECUPERAÇÃO É ENTRE 5 E 7, E PARA REPROVAÇÃO É MENOR QUE 5
+
+const notasAluno = [8, 7, 9, 6, 10, 8, 6, 5, 3 ,9, 10, 7, 8, 6, 5, 4, 9, 10, 8, 7, 6]
+
+let mediaAluno = 0
+
+for (const nota in notasAluno) {
+    mediaAluno = mediaAluno + notasAluno[nota]
+}
+
+mediaAluno = mediaAluno / notasAluno.length
+
+
+if (mediaAluno >= 7) {
+    console.log("SUA NOTA FOI: ", mediaAluno);
+    console.log("PARABENS VOCE FOI APROVADO");
+    
+} else if (mediaAluno >= 5 && mediaAluno < 7) {
+    console.log("SUA NOTA FOI: ", mediaAluno);
+    console.log("VOCE ESTA DE RECUPERACAO");
+} else {
+    console.log("SUA NOTA FOI: ", mediaAluno);
+    console.log("VOCE FOI REPROVADO");
+}
+
+
+
+
+
+
